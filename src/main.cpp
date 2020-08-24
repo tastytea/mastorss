ec3c8933b2686285852eef02b74809426411144f
tastytea <tastytea@tastytea.de>
2020-08-24 16:23:24 +0200

    Add license header to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+/*  This file is part of mastorss.
+ *  Copyright © 2019, 2020 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "config.hpp"
 #include "document.hpp"
 #include "exceptions.hpp"
